0822b4efb3648b1849279e49d4c7757c54b1c3e1
simonetibaldi <simone.tibaldi95@gmail.com>
2021-07-03 11:47:15 +0200

    Tolto cost.py, messo più funzioni in quantum_loop che fa anche ottimizzazione adesso, tolto molta roba importata ridontante (ce n'è ancora da togliere) ma adesso funziona

--- utils/quantumloop.py
+++ utils/quantumloop.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from pulser import Pulse, Sequence, Register, Simulation
 from pulser.devices import Chadoq2
 
-def quantum_loop(param, r=reg):
+def quantum_loop(param, r):
     seq = Sequence(r, Chadoq2)
     seq.declare_channel('ch0','rydberg_global')
     middle = int(len(param)/2)
@@ -23,3 +23,31 @@ def quantum_loop(param, r=reg):
     results = simul.run()
     count_dict = results.sample_final_state(N_samples=1000) #sample from the state vector
     return count_dict
+
+def get_cost_colouring(z,G,penalty=10):
+    """G: the graph (igraph)
+       z: a binary colouring
+       returns the cost of the colouring z, depending on the adjacency of the graph"""
+    cost = 0
+    A = G.get_adjacency()
+    z = np.array(tuple(z),dtype=int)
+    for i in range(len(z)):
+        for j in range(i,len(z)):
+            cost += A[i][j]*z[i]*z[j]*penalty # if there's an edge between i,j and they are both in |1> state.
+
+    cost -= np.sum(z) #to count for the 0s instead of the 1s
+    return cost
+
+def get_cost_state(counter,G):
+    cost = 0
+    for key in counter.keys():
+        cost_col = get_cost_colouring(key,G)
+        cost += cost_col * counter[key]
+    return cost / sum(counter.values())
+
+def func(param,*args):
+    G = args[0]
+    C = quantum_loop(param, r=args[1])
+    cost = get_cost_state(C,G)
+    return cost
+
