ce6690c2c9e4e18d3e07c4cc8fba15aa2e0d6bcd
Davide <davide.vodola@unibo.it>
2021-11-23 12:49:26 +0100

    Modify plot function for chair graph

--- utils/create_graphs.py
+++ utils/create_graphs.py
@@ -22,23 +22,15 @@ def create_random_graph(num_nodes, average_connectivity, name_plot=False):
     return G
 
 
-def create_chair_graph(draw=False):
+def create_chair_graph(name_plot=False):
     num_nodes = 6
     G = nx.Graph()
     pos = np.array([[0, 1], [1, 2], [2, 3], [0, 3], [0, 4], [0,5]])
     G.add_edges_from(pos)
-    graph_name_file = "chair"
 
-    nx.draw(G, pos=pos, with_labels=True)
-    plt.savefig(graph_name_file + ".pdf")
-    nx.write_edgelist(G, graph_name_file + ".edgelist", data=True)
-    nx.write_edgelist(G, graph_name_file + "false.edgelist", data=False)
-    mapping = dict([[j,j] for j in range(num_nodes)])
-    H =  nx.relabel_nodes(G, mapping)
-    data_graph = json_graph.node_link_data(H)
-    out_file = open(graph_name_file + ".json", "w")
-    json_object = json.dump(data_graph, out_file)
-    out_file.close()
+    if name_plot:
+        graph_name_file = "chair"
+        _plot_graph(G, graph_name_file)
 
     return G
 
