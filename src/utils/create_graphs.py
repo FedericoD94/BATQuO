6caae1347d716d8bf667a2cf207f30466b35fe83
Edoardo Tignone <edoardo.tignone@leitha.eu>
2021-12-20 18:32:00 +0100

    add func to create graph for use case

--- src/utils/create_graphs.py
+++ src/utils/create_graphs.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from networkx.linalg.graphmatrix import adjacency_matrix
 from pathlib import Path
+from typing import List
 
 def create_random_graph(num_nodes, average_connectivity, name_plot=False):
     G = nx.Graph()
@@ -38,6 +39,47 @@ def create_random_regular_graph(num_nodes, degree=3, seed=1, name_plot=False):
     if name_plot:
         graph_name_file = str(name_plot)
         _plot_graph(G, graph_name_file)
+    return G
+
+
+def create_graph_usecase_insurance(n_customers: int,
+                                   n_renewal_ratios_bins: int,
+                                   previous_premiums: List[float],
+                                   renewal_ratios_bins: List[float],
+                                   acceptance_probabilities: List[List[float]],
+                                   penalty_terms: List[float]):
+    
+    '''
+    creates a graph for the portfolio renewal usecase of Leithà
+    '''
+
+    if type(acceptance_probabilities) == list:
+        acceptance_probabilities = np.array(acceptance_probabilities)
+
+    # check data dimensions
+    assert len(penalty_terms) == n_customers; 'there should be as many penalty terms as customers'
+    assert len(penalty_terms) == len(previous_premiums); 'there should be as many penalty terms as previous premiums'
+    assert len(acceptance_probabilities) == n_customers; f'there should {n_customers*n_renewal_ratios_bins} acceptance_probabilities'
+    assert len(acceptance_probabilities[0]) == n_renewal_ratios_bins; f'there should {n_customers*n_renewal_ratios_bins} acceptance_probabilities'
+    assert np.all(acceptance_probabilities.sum(axis=1)); 'acceptance_probabilities are not summing to 1 for at least one customer'
+    
+    # build unweighted graph  
+    G = nx.Graph()
+    for c in range(n_customers):
+        tmp_circular_G = nx.cycle_graph(n_renewal_ratios_bins)
+        tmp_circular_G = nx.relabel_nodes(tmp_circular_G, {n: (c,n) for n in tmp_circular_G.nodes})
+        G = nx.compose(G,tmp_circular_G)
+
+    # add node weights
+    for n,d in G.nodes(data=True):
+        i,j = n
+        d['weight'] = acceptance_probabilities[i][j] * (1 + previous_premiums[i] * renewal_ratios_bins[j]) + 2 * penalty_terms[i]
+
+    # add edge weights
+    for u,v in G.edges:
+        i = u[0]
+        G[u][v]['weight']=penalty_terms[i]
+
     return G
 
 
