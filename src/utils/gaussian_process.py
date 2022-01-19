4896366fa1728bcd593a11daaacdf275eafe208e
Simone Tibaldi <simone.tibaldi95@gmail.comm>
2022-01-19 10:38:04 +0100

    small change6

--- src/utils/gaussian_process.py
+++ src/utils/gaussian_process.py
@@ -301,13 +301,16 @@ class MyGaussianProcessRegressor(GaussianProcessRegressor):
 
         samples = tfp.mcmc.sample_chain(
                                     num_results=1000,
-                                    current_state = np.zeros(len(self.kernel_.theta)),
+                                    current_state = np.ones(len(self.kernel_.theta)),
                                     kernel=tfp.mcmc.SliceSampler(self.log_marginal_likelihood,
                                                     step_size=1.0,
                                                     max_doublings=5),
                                     num_burnin_steps=500,
                                     trace_fn=None,
                                     seed=1234)
+        print(samples.type)
+        print(samples[:3])
+        exit()
         samples = samples[-N_points:] #taking the last N_points
         
         return samples
